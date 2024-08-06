4023fabf42379be848ae312a962a55b220c2a0c5
Justin Xiang <jx372@cornell.edu>
2024-08-05 22:52:43 -0400

    Add countown and new page

--- scripts/main.js
+++ scripts/main.js
@@ -32,3 +32,26 @@ if (!localStorage.getItem("name")) {
 myButton.onclick = () => {
   setUserName();
 };
+
+// Countdown Timer
+const countdownElement = document.getElementById('countdown-timer');
+const closingCeremonyDate = new Date('2024-08-11').getTime()
+
+function updateCountdown() {
+  const now = new Date().getTime();
+  const distance = closingCeremonyDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60) ));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+  if (distance < 0) {
+    clearInterval(countdownInterval);
+    countdownElement.innerHTML = "The 2024 Olympics have concluded."
+  }
+}
+
+const countdownInterval = setInterval(updateCountdown, 1000);
+updateCountdown();
